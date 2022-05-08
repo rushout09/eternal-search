62950cd9c371ba9015e8a6803c46c566eaa6f515
Rushabh Agarwal <rushabh.agarwal9@gmail.com>
2022-05-08 17:47:30 +0530

    1. persist user tokens with encryption. 2. refresh user tokens. 3. Google Docs search add condition check for 'domain' user. 4. Handle exception in search results.

--- ServiceProviders.py
+++ ServiceProviders.py
@@ -1,8 +1,16 @@
 import httpx
 import os
+import redis
 from dotenv import load_dotenv
+from datetime import datetime, timezone
+from httpx_oauth.oauth2 import OAuth2
+from oauthlib.common import UNICODE_ASCII_CHARACTER_SET
+from random import SystemRandom
+from cryptography.fernet import Fernet
 
 load_dotenv()
+store = redis.Redis()
+cipher = Fernet(Fernet.generate_key())
 
 # HOST_URL: str = 'https://rushabh.loca.lt'
 HOST_URL = os.getenv('HOST_URL')
@@ -11,6 +19,7 @@ timeout = httpx.Timeout(10.0)
 
 
 class BaseServiceProvider:
+    NAME: str
     CLIENT_ID: str
     CLIENT_SECRET: str
     REDIRECT_URI: str
@@ -18,10 +27,77 @@ class BaseServiceProvider:
     SCOPES: str
     AUTH_URL: str
     TOKEN_URL: str
+    REFRESH_URL: str
+    oauth: OAuth2
 
-    @staticmethod
-    async def search(search_term: str, client: httpx.AsyncClient, api_url: str, headers: dict, **kwargs) -> list:
+    @classmethod
+    async def search(cls, search_term: str, client: httpx.AsyncClient, api_url: str, headers: dict, **kwargs) -> list:
         raise NotImplementedError
+
+    @classmethod
+    async def get_access_token(cls):
+        print(cls)
+        print(cls.NAME)
+        if store.hget(cls.NAME, 'ACCESS'):
+            access_token = cipher.decrypt((store.hget(cls.NAME, 'ACCESS'))).decode("utf-8")
+            expiry_time = int(store.hget(cls.NAME, 'EXPIRES_AT').decode("utf-8"))
+            if expiry_time < int(round(datetime.now(tz=timezone.utc).timestamp())):
+                await cls.refresh_token()
+
+                access_token = cipher.decrypt((store.hget(cls.NAME, 'ACCESS'))).decode("utf-8")
+            return access_token
+        return None
+
+    @classmethod
+    def persist_oauth_token(cls, oauth2_token: dict):
+
+        print(cls)
+        print(cls.NAME)
+        access_token = oauth2_token.get('access_token')
+        refresh_token = oauth2_token.get('refresh_token')
+        expires_at = oauth2_token.get('expires_in') + int(round(datetime.now(tz=timezone.utc).timestamp()))
+        scopes = oauth2_token.get('scope')
+
+        store.hset(cls.NAME, "ACCESS", cipher.encrypt(access_token.encode("utf-8")))
+        store.hset(cls.NAME, "REFRESH", cipher.encrypt(refresh_token.encode("utf-8")))
+        store.hset(cls.NAME, "EXPIRES_AT", str(expires_at))
+        store.hset(cls.NAME, "SCOPES", scopes)
+
+    @classmethod
+    async def refresh_token(cls):
+
+        print(cls)
+        print(cls.NAME)
+        refresh_token = cipher.decrypt((store.hget(cls.NAME, 'REFRESH'))).decode("utf-8")
+        print("refreshing access token")
+        oauth2_token = await cls.oauth.refresh_token(refresh_token=refresh_token)
+        cls.persist_oauth_token(oauth2_token)
+
+    @classmethod
+    async def get_authorization_url(cls, extras_params: dict):
+
+        return await cls.oauth.get_authorization_url(
+            redirect_uri=cls.REDIRECT_URL,
+            state=cls.generate_token(),
+            extras_params=extras_params)
+
+    @classmethod
+    async def get_initial_oauth_token(cls, code):
+
+        oauth2_token = await cls.oauth.get_access_token(code=code, redirect_uri=cls.REDIRECT_URL)
+        return oauth2_token
+
+    @classmethod
+    def generate_token(cls, length=30, chars=UNICODE_ASCII_CHARACTER_SET):
+        """Generates a non-guessable OAuth token
+
+        OAuth (1 and 2) does not specify the format of tokens except that they
+        should be strings of random characters. Tokens should not be guessable
+        and entropy when generating the random characters is important. Which is
+        why SystemRandom is used instead of the default random.choice method.
+        """
+        rand = SystemRandom()
+        return ''.join(rand.choice(chars) for x in range(length))
 
 
 class GoogleServiceProvider(BaseServiceProvider):
@@ -35,25 +111,59 @@ class GoogleServiceProvider(BaseServiceProvider):
     AUTH_URL: str = 'https://accounts.google.com/o/oauth2/v2/auth'
     TOKEN_URL: str = 'https://www.googleapis.com/oauth2/v4/token'
     REFRESH_URL: str = 'https://www.googleapis.com/oauth2/v4/token'
+    GDRIVE_API_URL: str = 'https://www.googleapis.com/drive/v3/files'
+    oauth: OAuth2 = OAuth2(
+        name=NAME,
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        authorize_endpoint=AUTH_URL,
+        access_token_endpoint=TOKEN_URL,
+        refresh_token_endpoint=REFRESH_URL,
+        base_scopes=SCOPES)
 
-    @staticmethod
-    async def search(search_term: str, client: httpx.AsyncClient, api_url: str, headers: dict, **kwargs) -> list:
-
+    @classmethod
+    async def search(cls, search_term: str, access_token: str, **kwargs) -> list:
         # corpora should be not sent if the user does not belong to any enterprise domain.
         gdrive_params = {
-            'q': f'fullText contains "{search_term}"'
+            'q': f'fullText contains "{search_term}"',
+            'corpora': 'domain'
         }
-        gdrive_response: httpx.Response = await client.get(url=api_url, params=gdrive_params, headers=headers,
-                                                           timeout=timeout)
-        print("GDrive response is: " + str(gdrive_response.json()))
-        gdrive_response_list = gdrive_response.json()['files']
-        search_results = []
-        for result in gdrive_response_list:
-            search_results.append({
-                'title': result.get('name'),
-                'type': result.get('mimeType')
-            })
-        return search_results
+        headers = {'Authorization': f"Bearer {access_token}",
+                   'Accept': 'application/json'}
+
+        retry = True
+
+        try:
+
+            async with httpx.AsyncClient() as client:
+
+                while retry:
+                    response: httpx.Response = await client.get(url=cls.GDRIVE_API_URL, params=gdrive_params,
+                                                                headers=headers,
+                                                                timeout=timeout)
+                    if response.status_code == 200:
+                        retry = False
+                    elif response.status_code == 400:
+                        gdrive_params = {
+                            'q': f'fullText contains "{search_term}"'
+                        }
+                    elif response.status_code == 401:
+                        await cls.refresh_token()
+                    else:
+                        raise ValueError("Invalid Response")
+
+            print("GDrive response is: " + str(response.json()))
+            gdrive_response_list = response.json()['files']
+            search_results = []
+            for result in gdrive_response_list:
+                search_results.append({
+                    'title': result.get('name'),
+                    'type': result.get('mimeType')
+                })
+            return search_results
+        except ValueError:
+            print(str(ValueError))
+            return []
 
 
 class AtlassianServiceProvider(BaseServiceProvider):
@@ -67,30 +177,59 @@ class AtlassianServiceProvider(BaseServiceProvider):
     AUTH_URL: str = 'https://auth.atlassian.com/authorize'
     TOKEN_URL: str = 'https://auth.atlassian.com/oauth/token'
     REFRESH_URL: str = 'https://auth.atlassian.com/oauth/token'
+    CONFLUENCE_API_URL: str = 'https://api.atlassian.com/ex/confluence'
+    oauth: OAuth2 = OAuth2(
+        name=NAME,
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        authorize_endpoint=AUTH_URL,
+        access_token_endpoint=TOKEN_URL,
+        refresh_token_endpoint=REFRESH_URL,
+        base_scopes=SCOPES)
 
-    @staticmethod
-    async def search(search_term: str, client: httpx.AsyncClient, api_url: str, headers: dict,  **kwargs) -> list:
+    @classmethod
+    async def search(cls, search_term: str, access_token: str, **kwargs) -> list:
         query = f'text~{search_term}'
-        confluence_response = await client.get(
-            url=f"{api_url}/{kwargs.get('confluence_cloud_id')}/wiki/rest/api/search",
-            params={
-                'cql': query
-            },
-            headers=headers,
-            timeout=timeout)
+        headers = {'Authorization': f"Bearer {access_token}",
+                   'Accept': 'application/json'}
 
-        confluence_results: list = confluence_response.json()['results']
+        retry = True
 
-        print("confluence response is: " + str(confluence_results))
-        search_results = []
-        for result in confluence_results:
-            search_results.append({
-                        'excerpt': result['excerpt'],
-                        'title': result['title'],
-                        'link': result['url']
-                    })
-            print(result)
-        return search_results
+        try:
+            async with httpx.AsyncClient() as client:
+
+                while retry:
+                    response: httpx.Response = await client.get(
+                        url=f"{cls.CONFLUENCE_API_URL}/{kwargs.get('confluence_cloud_id')}/wiki/rest/api/search",
+                        params={
+                            'cql': query
+                        },
+                        headers=headers,
+                        timeout=timeout)
+
+                    # Confirm if status code for expired access token is correct.
+                    if response.status_code == 200:
+                        retry = False
+                    elif response.status_code == 403:
+                        await cls.refresh_token()
+                    else:
+                        raise ValueError("Invalid Response")
+
+            confluence_results: list = response.json()['results']
+            print("confluence response is: " + str(confluence_results))
+            search_results = []
+            for result in confluence_results:
+                search_results.append({
+                    'excerpt': result['excerpt'],
+                    'title': result['title'],
+                    'link': result['url']
+                })
+                print(result)
+            return search_results
+
+        except ValueError:
+            print(str(ValueError))
+            return []
 
 
 class SlackServiceProvider(BaseServiceProvider):
@@ -104,22 +243,52 @@ class SlackServiceProvider(BaseServiceProvider):
     AUTH_URL: str = 'https://slack.com/oauth/v2/authorize'
     TOKEN_URL: str = 'https://slack.com/api/oauth.v2.access'
     REFRESH_URL: str = 'https://slack.com/api/oauth.v2.access'
+    SLACK_API_URL: str = 'https://slack.com/api/search.all'
+    oauth: OAuth2 = OAuth2(
+        name=NAME,
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        authorize_endpoint=AUTH_URL,
+        access_token_endpoint=TOKEN_URL,
+        refresh_token_endpoint=REFRESH_URL,
+        base_scopes=SCOPES)
 
-    @staticmethod
-    async def search(search_term: str, client: httpx.AsyncClient, api_url: str, headers: dict, **kwargs) -> list:
-        slack_response = await client.get(url=f"{api_url}", params={'query': search_term},
-                                          headers=headers,
-                                          timeout=timeout)
-        print("slack response is: " + str(slack_response.json()))
-        slack_results: list = slack_response.json()['messages']['matches']
-        search_results = []
-        for result in slack_results:
-            search_results.append({
-                'username': result.get('username'),
-                'text': result.get('text'),
-                'link': result.get('permalink')
-            })
-        return search_results
+    @classmethod
+    async def search(cls, search_term: str, access_token: str, **kwargs) -> list:
+        headers = {'Authorization': f"Bearer {access_token}",
+                   'Accept': 'application/json'}
+
+        retry = True
+
+        try:
+            async with httpx.AsyncClient() as client:
+
+                while retry:
+                    response: httpx.Response = await client.get(url=f"{cls.SLACK_API_URL}", params={'query': search_term},
+                                                                headers=headers,
+                                                                timeout=timeout)
+                    print("slack response is: " + str(response.json()))
+                    response_json = response.json()
+                    if response_json['ok'] is True:
+                        retry = False
+                    elif response_json['ok'] is False and response_json['error'] == "invalid_auth":
+                        await cls.refresh_token()
+                    else:
+                        raise ValueError("Invalid Response")
+
+            slack_results: list = response.json()['messages']['matches']
+            search_results = []
+            for result in slack_results:
+                search_results.append({
+                    'username': result.get('username'),
+                    'text': result.get('text'),
+                    'link': result.get('permalink')
+                })
+            return search_results
+
+        except ValueError:
+            print(str(ValueError))
+            return []
 
     @staticmethod
     def fix_access_token(params: dict) -> dict:
--- main.py
+++ main.py
@@ -4,46 +4,30 @@ import uvicorn
 import httpx
 import redis
 from random import SystemRandom
-from typing import Optional
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
-from httpx_oauth.oauth2 import OAuth2
 from oauthlib.common import UNICODE_ASCII_CHARACTER_SET
 from starlette.datastructures import ImmutableMultiDict
-from ServiceProviders import AtlassianServiceProvider, GoogleServiceProvider, SlackServiceProvider
+from ServiceProviders import AtlassianServiceProvider, GoogleServiceProvider, SlackServiceProvider, BaseServiceProvider
 
 app = FastAPI()
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 httpxClient = httpx.AsyncClient()
 store = redis.Redis()
 
-CONFLUENCE_API_URL = 'https://api.atlassian.com/ex/confluence'
-oauth_atlassian: Optional[OAuth2] = None
-
-GDRIVE_API_URL = 'https://www.googleapis.com/drive/v3/files'
-oauth_google: Optional[OAuth2] = None
-
-SLACK_API_URL = 'https://slack.com/api/search.all'
-oauth_slack: Optional[OAuth2] = None
-
 
 # currently this app is user agnostic. we will have to make it in such a way that user sign into or platform,
 # he will get buttons to authorize all other apps. but they will authorize only for that particular user by default.
 # currently we assume that user is already signed in.
-#
-# Todo: Research how option to refresh token works
-# Todo: Improve 'state' design as mentioned in requests_oauthlib
-# Todo: Research how to persist user tokens. p0
-# Todo: Research how to refresh user tokens. p0
+
+# Todo: Improve 'state' design as mentioned in requests_oauthlib to improve security.
 # Todo: Research way to invalidate token.
 # Todo: Research way to show token status.
 # Todo: Add Gmail search.
 # Todo: Add Jira search.
 # Todo: Add Github search.
-# Todo: Sort search results according to relevance.
+# Todo: Sort search results according to relevance. p0
 # Todo: beautify search results. p0
-# Todo: Google Docs search add condition check for 'domain' user. p0
-# Todo: Handle exception in search results.
 
 
 @app.get('/')
@@ -68,108 +52,56 @@ def home():
 
 @app.get('/authorize-atlassian')
 async def authorize_atlassian():
-    global oauth_atlassian
-    oauth_atlassian = OAuth2(
-        name=AtlassianServiceProvider.NAME,
-        client_id=AtlassianServiceProvider.CLIENT_ID,
-        client_secret=AtlassianServiceProvider.CLIENT_SECRET,
-        authorize_endpoint=AtlassianServiceProvider.AUTH_URL,
-        access_token_endpoint=AtlassianServiceProvider.TOKEN_URL,
-        refresh_token_endpoint=AtlassianServiceProvider.REFRESH_URL,
-        base_scopes=AtlassianServiceProvider.SCOPES)
-    authorization_url = await oauth_atlassian.get_authorization_url(redirect_uri=AtlassianServiceProvider.REDIRECT_URL,
-                                                                    state=generate_token(),
-                                                                    extras_params={'prompt': 'consent',
-                                                                                   'audience': 'api.atlassian.com'})
+    authorization_url = await AtlassianServiceProvider.get_authorization_url(
+        extras_params={'prompt': 'consent',
+                       'audience': 'api.atlassian.com'})
     return RedirectResponse(authorization_url)
 
 
 @app.get('/authorize-google')
 async def authorize_google():
-    global oauth_google
-    oauth_google = OAuth2(
-        name=GoogleServiceProvider.NAME,
-        client_id=GoogleServiceProvider.CLIENT_ID,
-        client_secret=GoogleServiceProvider.CLIENT_SECRET,
-        authorize_endpoint=GoogleServiceProvider.AUTH_URL,
-        access_token_endpoint=GoogleServiceProvider.TOKEN_URL,
-        refresh_token_endpoint=GoogleServiceProvider.REFRESH_URL,
-        base_scopes=GoogleServiceProvider.SCOPES)
-    authorization_url = await oauth_google.get_authorization_url(redirect_uri=GoogleServiceProvider.REDIRECT_URL,
-                                                                 state=generate_token(),
-                                                                 extras_params={'prompt': 'consent',
-                                                                                'access_type': 'offline'})
+    authorization_url = await GoogleServiceProvider.get_authorization_url(
+        extras_params={'prompt': 'consent',
+                       'access_type': 'offline'})
     return RedirectResponse(authorization_url)
 
 
 @app.get('/authorize-slack')
 async def authorize_slack():
-    global oauth_slack
-    oauth_slack = OAuth2(
-        name=SlackServiceProvider.NAME,
-        client_id=SlackServiceProvider.CLIENT_ID,
-        client_secret=SlackServiceProvider.CLIENT_SECRET,
-        authorize_endpoint=SlackServiceProvider.AUTH_URL,
-        access_token_endpoint=SlackServiceProvider.TOKEN_URL,
-        refresh_token_endpoint=SlackServiceProvider.REFRESH_URL)
-    authorization_url = await oauth_slack.get_authorization_url(redirect_uri=SlackServiceProvider.REDIRECT_URL,
-                                                                state=generate_token(),
-                                                                extras_params={
-                                                                    'user_scope': SlackServiceProvider.USER_SCOPES})
+    authorization_url = await SlackServiceProvider.get_authorization_url(
+        extras_params={
+            'user_scope': SlackServiceProvider.USER_SCOPES})
     return RedirectResponse(authorization_url)
 
 
 @app.get(f'/{GoogleServiceProvider.REDIRECT_URI}')
 async def google_authorization_success(code: str):
-
-    oauth2_token = await oauth_google.get_access_token(code=code, redirect_uri=GoogleServiceProvider.REDIRECT_URL)
-
-    google_access_token = oauth2_token.get('access_token')
-    google_refresh_token = oauth2_token.get('refresh_token')
-
-    store.hset("GOOGLE", "ACCESS", google_access_token)
-    store.hset("GOOGLE", "REFRESH", google_access_token)
-
-    print(google_refresh_token)
-    print(google_access_token)
+    oauth2_token = await GoogleServiceProvider.get_initial_oauth_token(code=code)
+    GoogleServiceProvider.persist_oauth_token(oauth2_token=oauth2_token)
     return RedirectResponse('/home')
 
 
 @app.get(f'/{SlackServiceProvider.REDIRECT_URI}')
 async def slack_authorization_success(code: str):
-
-    improper_oauth2_token = await oauth_slack.get_access_token(code=code,
-                                                               redirect_uri=SlackServiceProvider.REDIRECT_URL)
-
-    oauth2_token = SlackServiceProvider.fix_access_token(improper_oauth2_token)
-
-    slack_access_token = oauth2_token.get('access_token')
-    slack_refresh_token = oauth2_token.get('refresh_token')
-
-    store.hset("SLACK", "ACCESS", slack_access_token)
-    store.hset("SLACK", "REFRESH", slack_refresh_token)
-
-    print(slack_refresh_token)
-    print(slack_access_token)
+    oauth2_token = await SlackServiceProvider.get_initial_oauth_token(code=code)
+    oauth2_token = SlackServiceProvider.fix_access_token(oauth2_token)
+    SlackServiceProvider.persist_oauth_token(oauth2_token=oauth2_token)
     return RedirectResponse('/home')
 
 
 @app.get(f'/{AtlassianServiceProvider.REDIRECT_URI}')
 async def atlassian_authorization_success(code: str):
-
-    oauth2_token = await oauth_atlassian.get_access_token(code=code, redirect_uri=AtlassianServiceProvider.REDIRECT_URL)
+    oauth2_token = await AtlassianServiceProvider.get_initial_oauth_token(code=code)
 
     atlassian_access_token = oauth2_token.get('access_token')
-    atlassian_refresh_token = oauth2_token.get('refresh_token')
 
     response: httpx.Response = await httpxClient.get(url='https://api.atlassian.com/oauth/token/accessible-resources',
                                                      headers={'Authorization': f"Bearer {atlassian_access_token}",
                                                               'Accept': 'application/json'})
-    confluence_cloud_id = response.json()[0]['id']
+    atlassian_cloud_id = response.json()[0]['id']
 
-    store.hset("ATLASSIAN", "ACCESS", atlassian_access_token)
-    store.hset("ATLASSIAN", "REFRESH", atlassian_refresh_token)
-    store.hset("ATLASSIAN", "CLOUD_ID", confluence_cloud_id)
+    AtlassianServiceProvider.persist_oauth_token(oauth2_token=oauth2_token)
+    store.hset("ATLASSIAN", "CLOUD_ID", str(atlassian_cloud_id))
 
     return RedirectResponse('/home')
 
@@ -188,40 +120,36 @@ async def search(request: Request):
 
     response = {
         "response_type": "in_channel",
-        "text": "Searching Eternity."
+        "text": f"Searching Eternity for {text}."
     }
 
     return response
 
 
 async def search_worker(text: str, response_url: str):
-
     print('inside search worker')
 
     complete_search_result = []
 
-    if oauth_slack:
-        slack_access_token = store.hget('SLACK', 'ACCESS')
-        headers = {'Authorization': f"Bearer {slack_access_token}",
-                   'Accept': 'application/json'}
-        slack_search_results = await SlackServiceProvider.search(search_term=text, client=httpxClient,
-                                                                 api_url=SLACK_API_URL, headers=headers)
+    slack_access_token = await SlackServiceProvider.get_access_token()
+    if slack_access_token:
+        slack_search_results = await SlackServiceProvider.search(search_term=text, access_token=slack_access_token)
         complete_search_result.append(slack_search_results)
-    if oauth_google:
-        google_access_token = store.hget('GOOGLE', 'ACCESS')
-        headers = {'Authorization': f"Bearer {google_access_token}",
-                   'Accept': 'application/json'}
-        gdrive_search_results = await GoogleServiceProvider.search(search_term=text, client=httpxClient,
-                                                                   api_url=GDRIVE_API_URL, headers=headers)
+
+    google_access_token = await GoogleServiceProvider.get_access_token()
+    if google_access_token:
+
+        gdrive_search_results = await GoogleServiceProvider.search(search_term=text, access_token=google_access_token)
         complete_search_result.append(gdrive_search_results)
-    if oauth_atlassian:
-        atlassian_access_token = store.hget('ATLASSIAN', 'ACCESS')
-        confluence_cloud_id = store.hget('ATLASSIAN', 'CLOUD_ID')
-        headers = {'Authorization': f"Bearer {atlassian_access_token}",
-                   'Accept': 'application/json'}
-        confluence_search_results = await AtlassianServiceProvider.search(search_term=text, client=httpxClient,
-                                                                          api_url=CONFLUENCE_API_URL, headers=headers,
-                                                                          confluence_cloud_id=confluence_cloud_id)
+
+    atlassian_access_token = await AtlassianServiceProvider.get_access_token()
+    if atlassian_access_token:
+
+        confluence_search_results = await AtlassianServiceProvider.search(search_term=text,
+                                                                          access_token=atlassian_access_token,
+                                                                          confluence_cloud_id=store.hget("ATLASSIAN",
+                                                                                                         "CLOUD_ID")
+                                                                          .decode("utf-8"))
         complete_search_result.append(confluence_search_results)
 
     print(f'Complete search results: {str(complete_search_result)}')
